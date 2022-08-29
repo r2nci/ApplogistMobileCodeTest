8606eb41bc6bcb1407b28f5b9bb56e635e69bb2b
r2nci <r.ikinci@gmail.com>
2022-08-30 00:26:38 +0300

    First Commit

---
+++ ApplogistMobileCodeTest/ApplogistMobileCodeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplogistMobileCodeTest
+//
+//  Created by Ramazan ikinci on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
